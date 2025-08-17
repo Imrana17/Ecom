ac3d6b24983ac417dfca2e21ad7f6f28451c985d
Imrana Salis <imranasalis17@gmail.com>
2025-08-17 16:06:27 +0000

    I updated the Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
+  base: '/Ecom/',
+    server: {
+    host: "0.0.0.0",
     port: 8080,
   },
   plugins: [
