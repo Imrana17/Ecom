3ae6cdec17816d8f410a22070f0531e44273cb11
Imrana Salis <imranasalis17@gmail.com>
2026-02-02 14:39:43 +0000

    delete the api backend that wasnt working

--- src/api/index.ts
+++
@@ -1,9 +0,0 @@
-// Export all API modules
-export * from './auth';
-export * from './menu';
-export * from './cart';
-export * from './orders';
-export * from './delivery';
-export * from './payments';
-export * from './password';
-export { default as apiClient } from './client';
